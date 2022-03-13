f4e2955c00f9ea49c7453554e3b37826685e696b
Albert Vo <albert.vo.crypto@gmail.com>
2022-03-13 21:57:41 +0000

    add AUTOMATION

--- AUTOML/AUTOMATION/flask_automl.py
+++ AUTOML/AUTOMATION/flask_automl.py
@@ -1,4 +1,3 @@
-# Extremely simple flask application, will display 'Hello World!' on the screen when you run it
 # Access it by running it, then going to whatever port its running on (It'll say which port it's running on).
 from flask import Flask
 app = Flask(__name__)
